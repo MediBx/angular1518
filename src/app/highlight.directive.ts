0a18f0debc23c4516536a338083d09aaf4447dca
MediBx <ofraor6@gmail.com>
2025-02-15 09:30:51 -0500

    done directives15 branch

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -3,7 +3,7 @@ import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]',
-  standalone: true // באנגולר 15 אין צורך להוסיף לכל הנחיה
+  standalone: true
 })
 export class HighlightDirective {
   constructor(private el: ElementRef, private renderer: Renderer2) {
